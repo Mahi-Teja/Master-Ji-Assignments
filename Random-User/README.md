9e8620db8d78d7b31dd13d737895cafaf6e0dff2
mahineeli <mahineeli899@gmail.com>
2024-08-12 19:02:59 +0530

    Random User Assinment

---
+++ Random-User/README.md
@@ -0,0 +1,7 @@
+# Random User - a MasterJi Assignment.
+
+### Api used:
+
+#### https://api.freeapi.app/api/v1/public/randomusers/user/random
+
+[ChaiCode](https://chaicode.com)
---
+++ Random-User/src/components/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from "./UserCard";
+
+// https://api.freeapi.app/api/v1/public/randomusers/user/random
+function App() {
+  return (
+    <>
+      <UserCard />
+    </>
+  );
+}
+
+export default App;
